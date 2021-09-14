2075049748c93cf459dbbb85549874d8acc978b9
Emilia <emiliaprzybylek@hotmail.com>
2021-09-14 18:18:07 +0200

    stars

---
+++ js/script.js
@@ -0,0 +1,30 @@
+' use strick';
+
+
+let star = '';
+
+for(let i = 0; i < 5; i++){
+    for(let j = 0; j < 5; j++){
+        star += '*';
+    }
+    star +='\n';
+}
+console.log(star);
+
+
+///////////////////////
+
+let stars = '';
+
+for(let i = 0; i <= 4; i++) {
+    stars += '*****\n';
+}
+console.log(stars);
+
+
+
+
+
+
+
+
